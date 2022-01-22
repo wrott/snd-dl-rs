8828de215c05ea4d641fe916270a6a6088f98f66
wrott <wrottkitkatt@gmail.com>
2022-01-22 15:49:29 +0200

    de

--- src/api.rs
+++ src/api.rs
@@ -6,12 +6,18 @@ use crate::models::{DownloadStreamResponse, DownloadTrackResponse, TrackInfo};
 const CLIENT_ID: &str = "a3e059563d7fd3372b49b37f00a00bcf";
 
 pub fn resolve_track_info(url_to_resolve: String) -> reqwest::Result<TrackInfo> {
-    let url: String = format!("https://api-v2.soundcloud.com/resolve?url={}&client_id={}", url_to_resolve, CLIENT_ID);
+    let url: String = format!(
+        "https://api-v2.soundcloud.com/resolve?url={}&client_id={}",
+        url_to_resolve, CLIENT_ID
+    );
     reqwest::blocking::get(url)?.json()
 }
 
 pub fn get_download_link(id: u64) -> reqwest::Result<DownloadTrackResponse> {
-    let url: String = format!("https://api-v2.soundcloud.com/tracks/{}/download?client_id={}", id, CLIENT_ID);
+    let url: String = format!(
+        "https://api-v2.soundcloud.com/tracks/{}/download?client_id={}",
+        id, CLIENT_ID
+    );
     let response: Response = reqwest::blocking::get(url)?;
     response.json()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::{env, thread};
 use std::cmp::min;
 use std::fs::File;
 use std::io::Write;
 use std::time::Duration;
+use std::{env, thread};
 
 use bytes::Bytes;
 use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::api::get_bytes;
 
-mod models;
 mod api;
 mod m3u8_parser;
+mod models;
 
 //const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -31,25 +31,26 @@ fn download_hls_track(url: String, name: String) {
     m3u8_parser::download_hls_stream_to_file(hls_url, &file);
 }
 
-
 fn main() {
     let mut downloaded = 0;
     let url = env::args().nth(1).expect("NEED AN URL SONG");
-    let track_info = api::resolve_track_info(url)
-        .expect("Failed to resolve track info");
+    let track_info = api::resolve_track_info(url).expect("Failed to resolve track info");
 
     if track_info.downloadable {
-        let raw_track = download_original_track(track_info.id)
-            .expect("Failed to download raw track");
-        save_track_locally(raw_track, track_info.permalink)
-            .expect("Failed to save raw track");
+        let raw_track =
+            download_original_track(track_info.id).expect("Failed to download raw track");
+        save_track_locally(raw_track, track_info.permalink).expect("Failed to save raw track");
         let size = 16;
         let pb = ProgressBar::new(size);
-        pb.set_style(ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] \
-        [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-            //.with_key("eta", |state| format!("{:.1}s", state.eta().as_secs_f64()))
-            .progress_chars("#>-"));
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template(
+                    "{spinner:.green} [{elapsed_precise}] \
+        [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})",
+                )
+                //.with_key("eta", |state| format!("{:.1}s", state.eta().as_secs_f64()))
+                .progress_chars("#>-"),
+        );
 
         while downloaded < size {
             let new = min(downloaded + 223211, size);
@@ -58,7 +59,8 @@ fn main() {
             thread::sleep(Duration::from_millis(12));
         }
     } else {
-        let stream_url = track_info.get_stream_url()
+        let stream_url = track_info
+            .get_stream_url()
             .expect("No original download or mpeg stream available for track");
         download_hls_track(stream_url, track_info.permalink)
     }
